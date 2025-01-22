094a3e8242e9c189dc56ce6fd1543daee8f888e8
hanskarlo <hansjarales@gmail.com>
2025-01-22 12:52:16 -0700

    SystemInterface method init override prototypes, defs, plugin export

--- include/ros2-serial-hw-interface/serial_hw_if.hpp
+++ include/ros2-serial-hw-interface/serial_hw_if.hpp
@@ -12,6 +12,30 @@
 
 #include "serial/serial.h"
 
+namespace serial_hw_if
+{
+
+    // using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+    class SerialHardwareInterface : public hardware_interface::SystemInterface
+    {
+        public:
+
+            hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo &info) override;
+
+            hardware_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State &previous_state) override;
+            hardware_interface::CallbackReturn on_cleanup(const rclcpp_lifecycle::State &previous_state) override;
+
+            hardware_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State &previous_state) override;
+            hardware_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State &previous_state) override;
+
+            hardware_interface::return_type read(const rclcpp::Time &time, const rclcpp::Duration &period) override;
+            hardware_interface::return_type write(const rclcpp::Time &time, const rclcpp::Duration &period) override;
+
+    };
+
+
+} // serial_hw_if
 
 
 
--- src/serial_hw_if.cpp
+++ src/serial_hw_if.cpp
@@ -1,1 +1,49 @@
 #include "../include/ros2-serial-hw-interface/serial_hw_if.hpp"
+#include "pluginlib/class_list_macros.hpp"
+
+namespace serial_hw_if
+{
+    hardware_interface::CallbackReturn SerialHardwareInterface::on_init(const hardware_interface::HardwareInfo &info)
+    {
+        
+    }
+
+
+    hardware_interface::CallbackReturn SerialHardwareInterface::on_configure(const rclcpp_lifecycle::State &previous_state)
+    {
+
+    }
+
+
+    hardware_interface::CallbackReturn SerialHardwareInterface::on_cleanup(const rclcpp_lifecycle::State &previous_state)
+    {
+
+    }
+
+
+    hardware_interface::CallbackReturn SerialHardwareInterface::on_activate(const rclcpp_lifecycle::State &previous_state)
+    {
+
+    }
+
+
+    hardware_interface::CallbackReturn SerialHardwareInterface::on_deactivate(const rclcpp_lifecycle::State &previous_state)
+    {
+
+    }
+
+
+    hardware_interface::return_type SerialHardwareInterface::read(const rclcpp::Time &time, const rclcpp::Duration &period)
+    {
+
+    }
+
+
+    hardware_interface::return_type SerialHardwareInterface::write(const rclcpp::Time &time, const rclcpp::Duration &period)
+    {
+
+    }
+
+}
+
+PLUGINLIB_EXPORT_CLASS(serial_hw_if::SerialHardwareInterface, hardware_interface::SystemInterface);
